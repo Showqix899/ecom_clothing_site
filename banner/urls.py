93857504f51f5d767b815d4ce84054e060f5d3f0
showqi <mshowqi12@gmail.com>
2025-12-27 00:03:50 +0600

    add image to hero section, landing_page , banner functionality added

--- banner/urls.py
+++ banner/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import add_banner,delete_banner,search_banner
+from .views import add_banner,delete_banner,search_banner,add_image_to
 urlpatterns = [
     path('add/',add_banner,name='add-banner'),
     path('delete/<str:banner_id>/',delete_banner,name='banner-delete'),
     path('search/',search_banner,name='search-banner'),
+    path('add-image-to/<str:type>/<str:banner_id>/',add_image_to,name='add-image-to'),    
 ]
--- banner/views.py
+++ banner/views.py
@@ -46,6 +46,10 @@ def add_banner(request):
         
         return JsonResponse({"error":"please add a title"})
     
+    is_existing = banner_col.find_one({'title':title})
+    if is_existing:
+        return JsonResponse({"error":"banner with this title already exists"})
+    
     banner_data['title'] = title
     
     if subtitle:
@@ -53,11 +57,12 @@ def add_banner(request):
         
     if not banner_image:
         return JsonResponse({"error":"please upload an image"})
+    
 
     try:
         banner_upload = cloudinary.uploader.upload(banner_image)
         if banner_upload and banner_upload.get('secure_url'):
-            banner_data['banner_url'] = banner_upload['secure_url']
+            banner_data['img_url'] = banner_upload['secure_url']
         else:
             return JsonResponse({"error":"image uploading failed"})
     except Exception as e:
@@ -128,8 +133,11 @@ def search_banner(request):
     # ---------- QUERY ----------
     title = request.GET.get('title')
     subtitle = request.GET.get('subtitle')
+    ban_id = request.GET.get('id')
     
     query = {}
+    if ban_id:
+        query['_id'] = ObjectId(ban_id)
     if title:
         query['title'] = title
     if subtitle:
@@ -149,5 +157,46 @@ def search_banner(request):
         return Response({"error": str(e)}, status=500)
     
     
+
     
+@api_view(['GET'])
+def list_banners(request):
+    try:
+        banners_cursor = banner_col.find()
+        banners_list = [serialize_banner(ban) for ban in banners_cursor]
+        count = len(banners_list)
+
+        return Response({
+            "count": count,
+            "banners": banners_list
+        })
+
+    except Exception as e:
+        return Response({"error": str(e)}, status=500)
+
+
     
+@api_view(['GET'])
+def add_image_to(request,type,banner_id):
+    
+    if request.method !='GET':
+        return JsonResponse({"error":f"{request.method} method not allowed"})
+    
+    user,error = get_current_user(request)
+    
+    if error:
+        return JsonResponse({"error":error})
+    
+    #checking user is admin or moderator
+    if not (is_user_admin(user) or is_user_moderator(user)):
+        return JsonResponse({'error': 'Unauthorized. Admin  or moderator access required.'}, status=403)
+    
+    if not type or type not in ['hero_section','landing_page','banner']:
+        return JsonResponse({"error":"please add a type from hero_section, landing_page, banner"})
+    
+    image = banner_col.find_one({'_id':ObjectId(banner_id)})
+    
+    if not image:
+        return JsonResponse({"error":"could not find the banner"})
+    
+    return JsonResponse({"image_url":image.get('banner_url')})

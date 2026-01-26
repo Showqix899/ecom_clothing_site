cc219cc32caf8e30b0e2be356ebb3e57d53565b4
showqi <mshowqi12@gmail.com>
2026-01-27 00:34:11 +0600

    django secrete moved to env

--- config/settings.py
+++ config/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=(8=f+y@n=o2i-f56-2wbfhsu$gn62z_zf@1133_#p&zn&jwb='
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False

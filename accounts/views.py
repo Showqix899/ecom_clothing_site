cea4d4c8362286f7c5c228813175d3f3b1bf4efd
showqi <mshowqi12@gmail.com>
2026-01-26 23:33:33 +0600

    role backed down admin creation

--- accounts/views.py
+++ accounts/views.py
@@ -342,7 +342,7 @@ def create_modarator_or_admin(request,user_id):
     role = body.get('role')
     
     
-    if role not in ['admin','moderator','user']:
+    if role not in ['admin','moderator']:
         return JsonResponse({"error":"role must be either 'admin' or 'moderator'"})
 
     #check if user exists
